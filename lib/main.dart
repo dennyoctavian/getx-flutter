a9957770f4e07985fda76371e58c0a3a9aac8642
Denny Octavian <dennyoctavian@Dennys-MacBook-Air.local>
2023-04-08 20:07:57 +0700

    done life cycle getx

--- lib/main.dart
+++ lib/main.dart
@@ -10,59 +10,70 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GetBuilder<CounterController>(
-                id: "joni",
-                init: CounterController(),
-                initState: (_) {},
-                builder: (controller) {
-                  return Text(
-                    "Angka ${controller.count}",
-                    style: const TextStyle(
-                      fontSize: 35,
-                    ),
-                  );
-                },
-              ),
-              GetBuilder<CounterController>(
-                init: CounterController(),
-                initState: (_) {},
-                builder: (controller) {
-                  return Text(
-                    "Angka ${controller.count}",
-                    style: const TextStyle(
-                      fontSize: 35,
-                    ),
-                  );
-                },
-              ),
-              GetBuilder<CounterController>(
-                id: "budi",
-                init: CounterController(),
-                initState: (_) {},
-                builder: (controller) {
-                  return Text(
-                    "Angka ${controller.count}",
-                    style: const TextStyle(
-                      fontSize: 35,
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Get.find<CounterController>().add();
-          },
-        ),
+    return const GetMaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final countController = Get.put(CounterController());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        actions: [
+          IconButton(
+              onPressed: () => Get.to(() => const OtherPage()),
+              icon: const Icon(Icons.refresh)),
+        ],
+      ),
+      body: const Center(
+        child: CountWidget(),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        countController.increment();
+      }),
+    );
+  }
+}
+
+class CountWidget extends StatelessWidget {
+  const CountWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<CounterController>(
+      initState: (state) => debugPrint('initState'),
+      didChangeDependencies: (state) => debugPrint('didChangeDependencies'),
+      didUpdateWidget: (oldWidget, state) => debugPrint('didUpdateWidget'),
+      dispose: (state) => debugPrint('dispose'),
+      builder: (controller) {
+        return Text("Angka ${controller.count}");
+      },
+    );
+  }
+}
+
+class OtherPage extends StatelessWidget {
+  const OtherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Other Page"),
+        actions: [
+          IconButton(
+              onPressed: () => Get.to(() => TextPage()),
+              icon: const Icon(Icons.refresh)),
+        ],
+      ),
+      body: const Center(
+        child: Text("Other Page"),
       ),
     );
   }
@@ -71,8 +82,30 @@ class MyApp extends StatelessWidget {
 class CounterController extends GetxController {
   var count = 0;
 
-  void add() {
+  void increment() {
     count++;
-    update(['joni', 'budi']);
+    update();
   }
+}
+
+class TextPage extends StatelessWidget {
+  TextPage({super.key});
+
+  final textC = Get.put(TextController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Text Page"),
+      ),
+      body: TextFormField(
+        controller: textC.myC,
+      ),
+    );
+  }
+}
+
+class TextController extends GetxController {
+  final myC = TextEditingController();
 }
