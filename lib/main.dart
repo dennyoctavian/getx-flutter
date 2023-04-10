f0fac872dd5bf36a710e13facfe21525d10dc2fb
Denny Octavian <dennyoctavian164@gmail.com>
2023-04-11 05:08:33 +0700

    getx dialog

--- lib/main.dart
+++ lib/main.dart
@@ -17,25 +17,42 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Builder(builder: (context) {
-                return ElevatedButton(
-                    onPressed: () {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          duration: Duration(seconds: 2),
-                          content: Text("hello")));
-
-                      // Get.snackbar("ini adalah judulnya", "ini adalah isi pesannya",
-                      //     animationDuration: const Duration(seconds: 2));
-                    },
-                    child: const Text("Scaffold Messenger"));
-              }),
               ElevatedButton(
                   onPressed: () {
-                    Get.snackbar(
-                        "ini adalah judulnya", "ini adalah isi pesannya",
-                        animationDuration: const Duration(seconds: 2));
+                    Get.dialog(
+                        barrierDismissible: false,
+                        const Dialog(
+                          child: Text("HELLO"),
+                        ));
                   },
-                  child: const Text("Getx snackbar"))
+                  child: const Text("Dialog")),
+              ElevatedButton(
+                  onPressed: () {
+                    Get.generalDialog(
+                        pageBuilder: (context, animation, secondAnimation) {
+                      return const AlertDialog();
+                    });
+                  },
+                  child: const Text("General Dialog")),
+              ElevatedButton(
+                  onPressed: () {
+                    Get.defaultDialog(
+                        barrierDismissible: false,
+                        onWillPop: () async => false,
+                        title: "Ini Judul",
+                        middleText: "Ini adalah dialog",
+                        // default
+                        textCancel: "Batal",
+                        cancelTextColor: Colors.pink,
+                        onCancel: () {},
+                        // custom
+                        cancel: ElevatedButton(
+                            onPressed: () {
+                              Get.back();
+                            },
+                            child: const Text("cancel")));
+                  },
+                  child: const Text("Default Dialog"))
             ],
           ),
         ),
