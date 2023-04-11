7ee2029bd51a6319cfe3d422c5c07c790ddfe426
Denny Octavian <dennyoctavian164@gmail.com>
2023-04-11 12:12:49 +0700

    getx bottom sheets done

--- lib/main.dart
+++ lib/main.dart
@@ -17,42 +17,52 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                  onPressed: () {
-                    Get.dialog(
-                        barrierDismissible: false,
-                        const Dialog(
-                          child: Text("HELLO"),
-                        ));
-                  },
-                  child: const Text("Dialog")),
-              ElevatedButton(
-                  onPressed: () {
-                    Get.generalDialog(
-                        pageBuilder: (context, animation, secondAnimation) {
-                      return const AlertDialog();
-                    });
-                  },
-                  child: const Text("General Dialog")),
-              ElevatedButton(
-                  onPressed: () {
-                    Get.defaultDialog(
-                        barrierDismissible: false,
-                        onWillPop: () async => false,
-                        title: "Ini Judul",
-                        middleText: "Ini adalah dialog",
-                        // default
-                        textCancel: "Batal",
-                        cancelTextColor: Colors.pink,
-                        onCancel: () {},
-                        // custom
-                        cancel: ElevatedButton(
-                            onPressed: () {
-                              Get.back();
-                            },
-                            child: const Text("cancel")));
-                  },
-                  child: const Text("Default Dialog"))
+              Builder(builder: (context) {
+                return ElevatedButton(
+                    onPressed: () {
+                      Get.bottomSheet(
+                          isScrollControlled: false,
+                          Padding(
+                            padding: EdgeInsets.only(
+                              bottom: MediaQuery.of(context).viewInsets.bottom,
+                            ),
+                            child: Container(
+                              color: Colors.amber,
+                              padding: const EdgeInsets.all(30),
+                              child: Center(
+                                child: ListView(
+                                  children: [
+                                    TextFormField(
+                                      decoration: const InputDecoration(
+                                          border: OutlineInputBorder()),
+                                    ),
+                                    TextFormField(
+                                      decoration: const InputDecoration(
+                                          border: OutlineInputBorder()),
+                                    ),
+                                    TextFormField(
+                                      decoration: const InputDecoration(
+                                          border: OutlineInputBorder()),
+                                    ),
+                                    TextFormField(
+                                      decoration: const InputDecoration(
+                                          border: OutlineInputBorder()),
+                                    ),
+                                    TextFormField(
+                                      decoration: const InputDecoration(
+                                          border: OutlineInputBorder()),
+                                    ),
+                                    ElevatedButton(
+                                        onPressed: () {},
+                                        child: const Text("Save"))
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ));
+                    },
+                    child: const Text("Dialog"));
+              }),
             ],
           ),
         ),
